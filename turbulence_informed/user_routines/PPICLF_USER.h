8ca6e32a15d9b0d965607d66567a25d270a9d2aa
arash-shad <54552356+arash-shad@users.noreply.github.com>
2026-01-06 12:26:34 -0500

    Add files via upload

---
+++ turbulence_informed/user_routines/PPICLF_USER.h
@@ -0,0 +1,37 @@
+#define PPICLF_STORE_LOCAL 1
+
+#define PPICLF_LRS 6
+#define PPICLF_LRP 19
+#define PPICLF_LEE 1000
+#define PPICLF_LEX 6
+#define PPICLF_LEY 6
+#define PPICLF_LEZ 6
+#define PPICLF_LRP_INT 3
+
+#define PPICLF_JX  1
+#define PPICLF_JY  2
+#define PPICLF_JZ  3
+#define PPICLF_JVX 4
+#define PPICLF_JVY 5
+#define PPICLF_JVZ 6
+
+#define PPICLF_R_JRHOP 1
+#define PPICLF_R_JDP   2
+#define PPICLF_R_JVOLP 3
+#define PPICLF_R_JUX   4
+#define PPICLF_R_JUY   5
+#define PPICLF_R_JUZ   6
+#define PPICLF_R_JDUX  7
+#define PPICLF_R_JDUY  8
+#define PPICLF_R_JDUZ  9
+#define PPICLF_R_JUTX  10
+#define PPICLF_R_JUTY  11
+#define PPICLF_R_JUTZ  12
+#define PPICLF_R_JVS 13
+#define PPICLF_R_JINJ 14
+#define PPICLF_R_JXI 15
+#define PPICLF_R_JYI 16
+#define PPICLF_R_JZI 17
+#define PPICLF_R_JVXI 18
+#define PPICLF_R_JVYI 19
+!#define PPICLF_R_JVZI 20
